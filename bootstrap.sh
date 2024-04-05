3922ccf2fb17b600c87c7110400c964bd3b09bd4
Ghilman Aji <139083228+ghilmanaji@users.noreply.github.com>
2024-04-05 10:10:52 +0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,3 @@
 export LIBP2P_FORCE_PNET=1
 ipfs bootstrap rm --all
-ipfs bootstrap add /ip4/45.77.38.105/tcp/4001/ipfs/12D3KooWPhNbzobvBJJgPLnDqAvx5vgAPzaqGw9DZnMSE9cp3qeD
-ipfs bootstrap add /ip4/139.180.133.85/tcp/4001/ipfs/12D3KooWDrQygcaENF2JXj2Wbsrm9k1y54QaYhhZrqmiVrDbpkiW
+ipfs bootstrap add /ip4/0.0.0.0/tcp/4001/ipfs/12D3KooWPhNbzobvBJJgPLnDqAvx5vgAPzaqGw9DZnMSE9cp3qez
